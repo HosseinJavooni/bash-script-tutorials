bc6a5e753c1bfbbf0a2814d6354b637e9a4febe0
h.javooni <h.javooni@faraboom.co>
2023-12-25 15:54:08 +0330

    ‫دوشنبه ۲۵ دسامبر ۲۳، ساعات ۱۵:۵۴:۰۸ (+0330)‬

---
+++ our_first_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Author: Hossein Jabani
+# Date Created: 2023-12-25
+# Last Modified: 2023-12-25
+
+# Description
+# Prints "This is my first script" to the terminal
+
+# Usage
+# our_first_script
+
+# This is a comment that start with # symbole
+
+echo "Hello world!"
+exit 0
