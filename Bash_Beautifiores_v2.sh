2a05ab64793a217fb29f9cd43f5cc9f9073f6f93
Hossein Jabani <h.javooni@gmail.com>
2024-01-05 00:52:37 +0330

    Fri Jan  5 12:52:37 AM +0330 2024

---
+++ Bash_Beautifiores_v2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#============MY_BASH_THEME=============
+parse_git_branch_unstage() {
+  result=$(git status 2> /dev/null)
+  if [[ $? = 0 && ($result == *"Untracked files:"* || $result == *"Changes not staged for commit:"*) ]]; then
+     echo " $(echo -e '\uE0A0') $(git branch --show-current) $(echo -e '\u26AB')"
+  else
+     echo ""
+  fi
+}
+parse_git_branch_staged() {
+  result=$(git status 2> /dev/null)
+  if [[ $? = 0 && $result != *"Untracked files:"* && $result != *"Changes not staged for commit:"* && $result == *"Changes to be committed:"* ]]; then
+     echo " $(echo -e '\uE0A0') $(git branch --show-current) $(echo -e '\u2716') "
+  else
+     echo ""
+  fi
+}
+parse_git_branch_committed() {
+  result=$(git status 2> /dev/null)
+  if [[ $? = 0 && $result == *"nothing to commit, working tree clean"* ]]; then
+     echo " $(echo -e '\uE0A0') $(git branch --show-current) $(echo -e '\u2714') "
+  else
+     echo ""
+  fi
+}
+
+PS1="\[\033[01;32m\]${USER}@${HOSTNAME}\[\033[00m\]:\[\033[01;34m\]\w\n\[\033[31m\]\$(parse_git_branch_unstage)\[\033[33m\]\$(parse_git_branch_staged)\[\033[32m\]\$(parse_git_branch_committed)\[\033[33m\]$(echo -e '\u25B6')\[\033[00m\] "
--- sampleTest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-result=$(git status 2> /dev/null)
-  if [[ $? = 0 && $result == *"Untracked files"* ]]; then
-     echo " $(echo -e '\uE0A0') $(git branch --show-current) "
-  else
-     echo ""
-  fi
-exit 0
